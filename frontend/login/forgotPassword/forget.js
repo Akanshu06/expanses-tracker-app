1fbff5948e4554d0a68b4bbfafeb9785d0ed9a19
Akanhu06 <akanshu3264@gmail.com>
2025-04-09 17:27:48 +0530

    hostchange

--- frontend/login/forgotPassword/forget.js
+++ frontend/login/forgotPassword/forget.js
@@ -1,16 +1,23 @@
-const form=document.querySelector('form');
-     
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const token = localStorage.getItem("token")
-    const mail =e.target.email.value;
-    const user={
-        mail:mail
-    }
-    axios.post('http://localhost:3000/password/forget-password',user,{
-        headers:{Authorization:token}
-    }).then((response)=>{
-        console.log(response);
-        document.body.innerHTML += '<div style="color:red;">Mail Successfuly sent <div>'
-    })
-})
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const token = localStorage.getItem("token");
+  const mail = e.target.email.value;
+  const user = {
+    mail: mail,
+  };
+  axios
+    .post(
+      "https://expanses-tracker-app-r.onrender.com/password/forget-password",
+      user,
+      {
+        headers: { Authorization: token },
+      }
+    )
+    .then((response) => {
+      console.log(response);
+      document.body.innerHTML +=
+        '<div style="color:red;">Mail Successfuly sent <div>';
+    });
+});
